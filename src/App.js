2239adb8e10106274c1bb5c9095bce196935cb07
marimm30 <marimacmoura@gmail.com>
2025-04-19 18:29:24 -0300

    simplificação do código: e pedido derevisão para o  uso de constantes

--- src/App.js
+++ src/App.js
@@ -4,28 +4,25 @@ import './App.css';
 
 class Usuario extends React.Component {
 
+/*Trocar o this.state pelo useState() - mais adequado ao projeto
+
   constructor(props){
     super(props)
     this.state ={
       email: 'eduardo.lino@pucpr.br',
       senha: '123456'
-    }
-  }
-}
+    }}}*/
 
-function ButtonAcessar() {
-  return (
-    <button>Acessar</button>
-  );
-}
 
 function App() {
+  
   return (
-      <h1 title="Login">
+    <div>
+      <h1 title="Login"/>
       <input type="email" id="email" value = {this.state.nome} placeholder="E-mail"/>
       <input type="password" id="password" value = {this.state.senha} placeholder="Senha"/>
-      <ButtonAcessar/>
-    </h1>
+      <button>Acessar</button>
+   </div>
   );
 }
 
